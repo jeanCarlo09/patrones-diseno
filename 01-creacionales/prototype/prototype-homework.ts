3a448ea1b15236ec980c57025f2d5805dad58035
Spectre09 <jvargas@broditec.net>
2025-03-18 20:40:18 -0600

    feat(prototype): implement prototype pattern

--- 01-creacionales/prototype/prototype-homework.ts
+++ 01-creacionales/prototype/prototype-homework.ts
@@ -11,19 +11,21 @@
  */
 
 class Pokemon {
-  name: string;
-  type: string;
-  level: number;
-  attacks: string[];
+  // name: string;
+  // type: string;
+  // level: number;
+  // attacks: string[];
 
-  constructor(name: string, type: string, level: number, attacks: string[]) {
-    throw new Error("Method not implemented.");
-  }
+  constructor(
+    public name: string,
+    public type: string,
+    public level: number,
+    public attacks: string[]
+  ) {}
 
-  // Método para clonar el Pokémon
+  // En el clone, debemos asegurar el deep clone, para evitar problemas con referencias
   clone(): Pokemon {
-    // Los ataques deben de evitar pasarse por referencia, es decir, no deben de ser el mismo arreglo.
-    // Completar: Debe devolver un nuevo Pokémon con los mismos atributos
+    return new Pokemon(this.name, this.type, this.level, [...this.attacks]);
   }
 
   displayInfo(): void {
@@ -40,12 +42,17 @@ class Pokemon {
 // 2. Clonar el Pokémon base y modificar algunos atributos en los clones.
 // 3. Llamar a displayInfo en cada Pokémon para mostrar sus detalles.
 
-// Ejemplo:
-// const basePokemon = new Pokemon("Charmander", "Fuego", 1, ["Llamarada", "Arañazo"]);
-// const clone1 = basePokemon.clone();
-// clone1.name = "Charmeleon";
-// clone1.level = 16;
-// clone1.attacks.push("Lanzallamas");
+(() => {
+  const basePokemon = new Pokemon("Charmander", "Fuego", 1, [
+    "Llamarada",
+    "Arañazo",
+  ]);
+  const clone1 = basePokemon.clone();
+  clone1.name = "Charmeleon";
+  clone1.level = 16;
+  clone1.attacks.push("Lanzallamas", "Gruñido");
+  clone1.attacks.pop();
 
-// basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
-// clone1.displayInfo();
+  basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
+  clone1.displayInfo();
+})();
--- 01-creacionales/prototype/prototype.ts
+++ 01-creacionales/prototype/prototype.ts
@@ -9,3 +9,49 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+class Document {
+  title: string;
+  private content: string;
+  author: string;
+
+  constructor(title: string, content: string, author: string) {
+    this.title = title;
+    this.content = content;
+    this.author = author;
+  }
+
+  clone(): Document {
+    return new Document(this.title, this.content, this.author);
+  }
+
+  displayInfo() {
+    console.log(
+      `
+        Title: ${this.title}
+        Content: ${this.content}
+        Author: ${this.author}
+        `
+    );
+  }
+}
+
+(() => {
+  const originalDocument = new Document(
+    "Cotización",
+    "Valor 500 dólares",
+    "Jean"
+  );
+
+  console.log({ originalDocument });
+  originalDocument.displayInfo();
+
+  //   const document2 = { ...originalDocument }; // structureClone(originalDocument);
+  //   document2.title = "Factura";
+  //   console.log({ document2 });
+
+  const document2 = originalDocument.clone();
+  document2.title = "Factura";
+  console.log({ document2 });
+  document2.displayInfo();
+})();
